6a61e4307bed5431a86d7ef91cd8b22ff74a84dd
Alex Schwartz <alexschwartz01@gmail.com>
2022-01-03 09:40:34 -0500

    Make a calculator console application

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Calculator");
+            Console.Write("x = ");
+            var x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y = ");
+            var y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{x} + {y} = {x+y}");
+            Console.WriteLine($"{x} - {y} = {x-y}");
+            Console.WriteLine($"{x} * {y} = {x*y}");
+            if(y == 0) {
+                Console.WriteLine($"{x} / {y} = undefined");
+            } else {
+                Console.WriteLine($"{x} / {y} = {x/y}");
+            }
         }
     }
 }
