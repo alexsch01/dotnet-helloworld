8fb752d7fccac0d0ff96381bd96c1bf583177fe8
Alex Schwartz <alexschwartz01@gmail.com>
2022-01-03 09:10:40 -0500

    write lines and get input from user

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.Write(Environment.NewLine);
+            Console.WriteLine($"Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+            Console.Write(Environment.NewLine);
         }
     }
 }
